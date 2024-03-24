5daa0ee9da42e1fd9635a34a10e0249c0acd1535
Alexander <tverdokhlebalex@me.com>
2024-03-24 11:21:37 +0400

    dev2 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState } from "react";
+import "./App.scss";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>ПРИВЕТ КЛОВЕРИ</div>
-      
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count +1 = {count}
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          count -1 = {count}
         </button>
       </div>
     </>
-  )
+  );
 }
-
-
