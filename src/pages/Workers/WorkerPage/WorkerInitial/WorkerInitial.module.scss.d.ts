6ff262147ad04a922a984248c803c99d66a32ab0
Alex_Che <chechalexey@mail.ru>
2024-04-08 19:30:48 +0300

    dev1_WD0LV-7_0804

--- src/pages/Workers/WorkerPage/WorkerInitial/WorkerInitial.module.scss.d.ts
+++
@@ -1,7 +0,0 @@
-declare const classNames: {
-  readonly workerInitial: "workerInitial";
-  readonly workerName: "workerName";
-  readonly workerMail: "workerMail";
-  readonly hidden: "hidden";
-};
-export = classNames;
--- src/pages/Workers/WorkerPage/buttons/buttons.module.scss.d.ts
+++
@@ -1,7 +0,0 @@
-declare const classNames: {
-  readonly banWorkerButton: "banWorkerButton";
-  readonly changeWorkerButton: "changeWorkerButton";
-  readonly deleteWorkerButton: "deleteWorkerButton";
-  readonly linkWorkerButton: "linkWorkerButton";
-};
-export = classNames;
