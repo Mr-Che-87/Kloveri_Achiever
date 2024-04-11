0aabe3c804660aea74cf2d775938b748fd629d74
Alex_Che <chechalexey@mail.ru>
2024-04-12 02:06:56 +0300

    dev1_WD0LV-7_1204_merge

--- src/pages/Workers/WorkerPage/WorkerAchivements/WorkerAchivements.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const classNames: {
-  readonly workerAchievements: "workerAchievements";
-  readonly workerAchievementsNav: "workerAchievementsNav";
-  readonly workerAchievementsList: "workerAchievementsList";
-  readonly achieveItem: "achieveItem";
-  readonly achieveTitle: "achieveTitle";
-  readonly removeButton: "removeButton";
-};
-export = classNames;
