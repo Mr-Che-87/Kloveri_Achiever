31aea3cafd6df563ee0510ed97cd343b9f8fdc22
Alexey Chechetkin <109634077+Mr-Che-87@users.noreply.github.com>
2024-04-26 18:48:32 +0300

    dev1_2604

--- src/pages/AchievementsConstructor/AchievementsConstructor.tsx
+++ src/pages/AchievementsConstructor/AchievementsConstructor.tsx
@@ -1,48 +1,25 @@
-
-/*
-export default function AchievementsConstructor() {
-
-return (
-  <div>блаблабла</div>
-)
-
-}
-*/
-
-
-//import axios from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import ModalAddingAchieve from "./ModalAddingAchieve";
+import { SearchAllAchieveInput } from "../Workers/WorkerPage/buttons&inputes/SearchAllAchieveInput";
 import styles from "../../pages/AchievementsConstructor/AchievementsConstructor.module.scss";
 import BookAvatar from "../../assets/book-icon.png";
-import { SearchAllAchieveInput } from "../Workers/WorkerPage/buttons&inputes/SearchAllAchieveInput";
-
 import { IAchieve } from "../../types/IAchieve";
-import { fetchGetAchieveLibrary } from "../../api/apiService";
 
 export default function AchievementsConstructor() {
   const [achievements, setAchievements] = useState<IAchieve[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-//GET-запрос achiev-lib(возвращает всю библиотеку наград):
-useEffect(() => {
-  //console.log("useEffect: загрузка всей библиотеки наград"); 
-  fetchGetAchieveLibrary()
-   .then((response) => {
-    //console.log("useEffect: Response всей библиотеки наград", response);
-    setAchievements(response.data);   //data - все данные из бэка{...}
-  })
-    .catch((error) => {
-      console.error("Ошибка при получении данных пользователя:", error);
-    });
-}, []);
-
-  /*
   useEffect(() => {
     const fetchAchievements = async () => {
       try {
         const response = await axios.get(
-          "https://reg.achiever.skroy.ru/achiev-lib/list/ "
+          "https://reg.achiever.skroy.ru/achiev-lib/list/"
         );
         setAchievements(response.data);
       } catch (error) {
@@ -52,7 +29,6 @@ useEffect(() => {
 
     fetchAchievements();
   }, []);
-  */
 
   return (
     <div className={styles.achievementsConstructor}>
@@ -63,11 +39,21 @@ useEffect(() => {
       <SearchAllAchieveInput
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
-  />
+      />
       <div className={styles.divider}></div>
       <div className={styles.achievementsGrid}>
-        {achievements.map((achievement: IAchieve) => (
-          <div key={achievement.id} className={styles.achievementCard}>
+        <button
+          onClick={toggleModal}
+          className={styles.createAchievementButton}
+        >
+          Создать достижение
+        </button>
+        {achievements.map((achievement) => (
+          <div
+            key={achievement.id}
+            className={styles.achievementCard}
+            style={{ backgroundImage: `url(${achievement.data.achiev_style})` }}
+          >
             <img src={achievement.data.image} alt={achievement.data.title} />
             <div className={styles.cardContent}>
               <h2>{achievement.data.title}</h2>
@@ -76,6 +62,7 @@ useEffect(() => {
           </div>
         ))}
       </div>
+      {isModalOpen && <ModalAddingAchieve closeModal={toggleModal} />}
     </div>
   );
 }
