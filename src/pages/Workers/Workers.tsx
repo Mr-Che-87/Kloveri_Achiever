7006cc48fc3756352df1c8b5aefb285445685947
Alexey Chechetkin <109634077+Mr-Che-87@users.noreply.github.com>
2024-05-16 22:15:04 +0300

    dev1_1605

--- src/pages/Workers/Workers.tsx
+++ src/pages/Workers/Workers.tsx
@@ -1,21 +1,44 @@
-import styles from "./Workers.module.scss";
-
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { IUser } from "../../types/IUser";
 import WorkerInitial from "./WorkerPage/WorkerInitial/WorkerInitial";
-
-import { fetchGetAllUsers } from "../../api/apiService";  //api
+import styles from "./Workers.module.scss";
+import { fetchGetAllUsers } from "../../api/apiService"; //api
 import iconHeader from "../../assets/iconsHeader.svg";
-import AddTeamButton from "../Teams/AddTeamButton/AddTeamButton";   //??? решить вопрос
+import AddTeamButton from "../Teams/AddTeamButton/AddTeamButton";
 import SearchInputWorkers from "./SearchInputWorkers/SearchInputWorkers";
-//import WorkersButtonAddTeam from "./WorkersButtonAddTeam/WorkersButtonAddTeam";
+import AuthorizationLinksButton from "./AuthorizationLinksButton/AuthorizationLinksButton";
+import WorkersButtonAddTeam from "./WorkersButtonAddTeam/WorkersButtonAddTeam";
+import WorkersModal from "./WorkersModal/WorkersModal";
+import WorkersModalAddUser from "./WorkersModalAddUser/WorkersModalAddUser";
 
+
+
+function filterName (searchTextName, nameList)  {
+  if(!searchTextName){
+    return nameList;
+  }
+  return nameList.filter(({first_name, last_name}) =>
+    [first_name, last_name].some(
+        (name) => name?.toLowerCase().includes(searchTextName.toLowerCase())
+    )
+   
+  )
+}
 
 export default function Workers() {
-  const [userList, setUserList] = useState<IUser[]>([]);  //state списка всех юзеров
+  const [userList, setUserList] = useState<IUser[]>([]); //state списка всех юзеров
+  const [isSearchName, setIsSearchName] = useState("");
+  const [isOpenModal, setIsOpenModal] = useState(false)
 
-//GET-Получение списка всех пользователей:
+  
+
+ const filtredUserList = filterName(isSearchName, userList)
+
+
+
+ console.log("filtredUserList", filtredUserList)
+  //GET-Получение списка всех пользователей:
   useEffect(() => {
     //const userId = "1";   //хз, как сейчас будут делить на admin | worker ???
     //console.log("useEffect: Загружен список всех пользователей");
@@ -35,61 +58,74 @@ export default function Workers() {
 
   return (
     <>
-    <div className={styles.workers}>
-      <div className={styles.workersTitle}>
-        <img src={iconHeader} alt="title icon" />
-        <h1> Сотрудники</h1>
-      </div>
-      <div className={styles.workersBtn}>
-        <AddTeamButton />
-        <SearchInputWorkers />
-      </div>
-
-      <div className={styles.workersCards}>
-        <div className={styles.workersList}>
-          <ul className={styles.workersList__item}>
-            {userList.map((user, index) => (  //МАПИМ МАССИВ ЮЗЕРОВ ИЗ БЭКА
-              <li key={index}>
-                <NavLink to={`/worker-page/${user.profile_id}`}>
-                  <WorkerInitial
-                    user={user} //передаем данные пользователя в WorkerInitial
-                    showEmail={false}
-                    avatarSize="small"
-                  />
-                </NavLink>
-                <div className={styles.workersTeamName}>
-                  <p>Название команды</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+      <div className={styles.workers}>
+        <div className={styles.workersTitle}>
+          <img src={iconHeader} alt="title icon" />
+          <h1> Сотрудники</h1>
+        </div>
+        <div className={styles.workersBtn}>
+          <AddTeamButton 
+          onClick={() => setIsOpenModal(true)}
+           />
+          <WorkersModal
+           isOpen={isOpenModal} 
+           onClose ={() => setIsOpenModal(false)}
+          />
+                  
+          <SearchInputWorkers  
+          isSearchName={isSearchName}
+          setIsSearchName={setIsSearchName}
+          />
+            
+         
+           {/* <WorkersModalAddUser/>  */}
+         
+            
         </div>
 
+        <div className={styles.workersCards}>
+          <div className={styles.workersList}>
+            
+            <ul className={styles.workersList__item}>
+              {filtredUserList.map((user, index) => (
+                <li key={index}>
+                  <NavLink to={`/worker-page/${user.profile_id}`}>
+                    <WorkerInitial
+                      user={user} //передаем данные пользователя в WorkerInitial
+                      showEmail={false}
+                      avatarSize="small"
+                    />
+                  </NavLink>
+                  <div className={styles.workersTeamName}>
+                    <p>Название команды</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        { //ПОКА НЕ НАДО:
-        /*  <div className={styles.workersNotInTheTeam}>
-          <p>НЕ В КОМАНДЕ</p>
-          <ul className={styles.workersList__item}>
-            {userList.map((user, index) => (
-              <li key={index}>
-                <NavLink to={`/worker-page/${user.profile_id}`}>
-                  <WorkerInitial
-                    user={user} //передаем данные пользователя в WorkerInitial
-                    showEmail={false}
-                    photoType="photo_small"
-                  />
-                </NavLink>
-                <div className={styles.workersAddTeam}>
-                  <WorkersButtonAddTeam/>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>   */}
-        
+          {/* <div className={styles.workersNotInTheTeam}>
+            <p>НЕ В КОМАНДЕ</p>
+            <ul className={styles.workersList__item}>
+              {userList.map((user, index) => (
+                <li key={index}>
+                  <NavLink to={`/worker-page/${user.profile_id}`}>
+                    <WorkerInitial
+                      user={user} //передаем данные пользователя в WorkerInitial
+                      showEmail={false}
+                      photoType="photo_small"
+                    />
+                  </NavLink>
+                  <div className={styles.workersAddTeam}>
+                    <WorkersButtonAddTeam/>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div> */}
+
+        </div>
       </div>
-    </div>
-  </>
-
+    </>
   );
 }
