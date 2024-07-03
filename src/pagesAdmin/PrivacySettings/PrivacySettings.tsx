3edbe5bbb555acdc107d9dee8df9c8fb67bb2bf2
Alexander <tverdokhlebalex@me.com>
2024-07-03 21:48:23 +0400

    dev2_0307

--- src/pagesAdmin/PrivacySettings/PrivacySettings.tsx
+++ src/pagesAdmin/PrivacySettings/PrivacySettings.tsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WorkerInitial from "../MyPage/WorkerInitial/WorkerInitial";
 import LogoutButton from "./LogoutButton";
 import ChangePassword from "./ChangePassword";
 import styles from "./PrivacySettings.module.scss";
 import { IUser } from "../../types/IUser";
+import WelcomeImg from "@/assets/Welcome-img.png";
 
 interface UserSettingsPageProps {
-  user: IUser | null;
+  userData: IUser | null;
 }
 
-const UserSettingsPage: React.FC<UserSettingsPageProps> = ({ user }) => {
-  console.log("User data:", user); // вывод для отладки
+const UserSettingsPage: React.FC<UserSettingsPageProps> = ({ userData }) => {
+  const [formData, setFormData] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    if (userData) {
+      setFormData({ ...userData });
+    }
+  }, [userData]);
 
   return (
     <div className={styles.userSettingsPage}>
       <h1>Настройки пользователя</h1>
-      {user ? (
+      <img src={WelcomeImg} alt="Welcome" />
+      {formData ? (
         <>
           <div className={styles.userFullName}>
-            {`${user.first_name || ""} ${user.middle_name || ""} ${user.last_name || ""}`}
+            {`${formData.first_name || ""} ${formData.middle_name || ""} ${formData.last_name || ""}`}
           </div>
-          <WorkerInitial user={user} showEmail={true} avatarSize="large" />
+          <WorkerInitial user={formData} showEmail={true} avatarSize="large" />
         </>
       ) : (
         <div>Загрузка данных пользователя...</div>
       )}
-      <LogoutButton />
+
       <ChangePassword />
+      <LogoutButton />
     </div>
   );
 };
