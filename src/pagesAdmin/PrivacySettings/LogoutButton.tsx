78851f8677255b6663d10015ccd5f0733c6c7b62
Alexander <tverdokhlebalex@me.com>
2024-07-04 20:44:35 +0400

    dev2_0507

--- src/pagesAdmin/PrivacySettings/LogoutButton.tsx
+++ src/pagesAdmin/PrivacySettings/LogoutButton.tsx
@@ -1,12 +1,16 @@
-import styles from "./LogoutButton.module.scss"
+import styles from "./LogoutButton.module.scss";
 
 function LogoutButton() {
   const handleLogout = () => {
-    localStorage.removeItem("authToken");
+    localStorage.clear();
     window.location.href = "/login"; // перенаправляем на страницу логина
   };
 
-  return <button className={styles.logoutButton} onClick={handleLogout}>Выйти</button>;
+  return (
+    <button className={styles.logoutButton} onClick={handleLogout}>
+      Выйти
+    </button>
+  );
 }
 
 export default LogoutButton;
