c4ef666d3d3a02107b417aed13689a5cdfed5b45
Alexander <tverdokhlebalex@me.com>
2024-07-18 22:18:35 +0400

    dev2_1807done

--- src/types/IUser.tsx
+++ src/types/IUser.tsx
@@ -2,28 +2,26 @@
 export interface IUser {
   link_id(link_id: any, formDataToSend: FormData): unknown;
   organization_id: string | undefined;
-  profile_id?: string | undefined;   
-  profile_type?: string | undefined;        //"anonymous"
+  profile_id?: string | undefined;
+  profile_type?: string | undefined; //"anonymous"
   profile_code?: string | undefined;
-  login?: string | undefined;        //"anonymous:a40a62fa-ee5e-4036-ae37-6c4329cffca1"
+  login?: string | undefined; //"anonymous:a40a62fa-ee5e-4036-ae37-6c4329cffca1"
   first_name?: string | undefined;
   last_name?: string | undefined;
   middle_name?: string | undefined;
   birth_date?: string | undefined;
-  sex?: string | undefined;     //удалить           //"male"||"female" строго 
-  phone?: string | undefined; 
-  email?: string | undefined;    
-  photo_main?: any;      //"https://i.ibb.co/dK1hhcD/big1.png"
-  photo_small?: any;   //"https://i.ibb.co/VwzhZgk/small1.png"
+  sex?: string | undefined; //удалить           //"male"||"female" строго
+  phone?: string | undefined;
+  email?: string | undefined;
+  photo_main?: any; //"https://i.ibb.co/dK1hhcD/big1.png"
+  photo_small?: any; //"https://i.ibb.co/VwzhZgk/small1.png"
   start_work_date?: string | undefined;
   specialty?: string | undefined;
   link_weight?: number | undefined;
-  project_id?: string | undefined; 
-  password? : string | undefined;
-     
-
+  project_id?: string | undefined;
+  password?: string | undefined;
+  rank?: number | undefined; // Добавляем новое свойство rank
 }
-
 
 //"https://www.example.com/media/imag/photo_main.jpg" - ПУСТОЙ url для теста
 
